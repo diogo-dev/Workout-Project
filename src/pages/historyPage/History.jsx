01bebcde165b9200946c88dd5dff5de41c6432be
Diogo Felipe <diogofelipe.dev@gmail.com>
2025-03-31 13:26:39 -0300

    Added Page: History

---
+++ src/pages/historyPage/History.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './History.css'
+import Timer from '../../components/timer/Timer'
+
+const History = () => {
+  return (
+    <div>
+      <Timer />
+    </div>
+  )
+}
+
+export default History
