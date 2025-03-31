eb7fe05bc2a78df871805cf58fa38df8e71af3d6
Diogo Felipe <diogofelipe.dev@gmail.com>
2025-03-31 13:12:44 -0300

    Updated: main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+/* Importing the provider */
+import { CalendarContextProvider } from './context/CalendarContext.jsx'
+import { PopUpProvider } from './context/PopUpContext.jsx'
+
+/* Remember that <StrictMode> runs useEffect() twice in development mode, but not in production mode */
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <CalendarContextProvider >
+      <PopUpProvider>
+        <App />
+      </PopUpProvider>
+    </CalendarContextProvider>
   </StrictMode>,
 )
