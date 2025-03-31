9378afee32242bc24036eee35d9b26912bb1ebbe
Diogo Felipe <diogofelipe.dev@gmail.com>
2025-03-31 13:02:26 -0300

    Added Component: NavBar

---
+++ src/components/navBar/NavBar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './NavBar.css'
+import { NavLink } from 'react-router-dom'
+import { CgGym } from "react-icons/cg"
+import DateSelector from '../../components/datePicker/DateSelector.jsx'
+
+
+const NavBar = () => {
+
+  return (
+    <header className='navbar-header'>
+      <NavLink to='/' className='navbar-logo'>Workout Planner <CgGym className='navbar-icon'/></NavLink>
+      
+      <nav className='navbar-links'>
+        <NavLink to='/'>Home</NavLink>
+        <NavLink to='/planner'>Planner</NavLink>
+        <NavLink to='/session'>Session</NavLink>
+        <NavLink to='/history'>History</NavLink>
+      </nav>
+    </header>
+  )
+}
+
+export default NavBar
